259ba6e5d778dbe7e6d1d896a9abb69800a63a97
Sharonah8 <nyamongosharon88@gmail.com>
2022-11-07 15:32:15 +0300

    Add categories navbar

--- client/vikali/src/Categories.js
+++ client/vikali/src/Categories.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 function Categories() {
-
+    <nav className="categories">
+        
+    </nav>
 }
 
 export default Categories

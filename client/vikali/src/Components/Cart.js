e41a50c6535b07132f5f01b1abc99503d305594b
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-11-08 00:41:20 +0300

    conditional filter

--- client/vikali/src/Components/Cart.js
+++ client/vikali/src/Components/Cart.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 function Cart() {
-  return (
-    <div>Cart</div>
-  )
+    return (
+        <div className='product-container'>
+            {products.filter((product) => product.discount >= 50)
+            }
+        </div>
+    );
 }
 
 export default Cart

87eb3d1700179872cc2e719673e5d8b78a8cb3c2
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-11-08 00:53:58 +0300

    debug

--- client/vikali/src/Components/Cart.js
+++ client/vikali/src/Components/Cart.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ProductCard from './ProductCard'
 
-function Cart() {
+function Cart({products}) {
     return (
         <div className='product-container'>
             {products.filter((product) => product.discount >= 50)
                 .map((product) => {
-                    return <ProductCard key={product.id} product={product}/>
+                    return <ProductCard key={product.id} product={product}/>;
             })}
         </div>
     );

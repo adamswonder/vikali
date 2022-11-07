49affa9672c282ee2011905284887ba855bfd147
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-11-07 15:48:27 +0300

    product info

--- client/vikali/src/ProductCard.js
+++ client/vikali/src/ProductCard.js
@@ -7,7 +7,12 @@ function ProductCard({product, cartIt}) {
         <span class="discount-tag">{product.discount}% off</span>
         <img src= {product.image} class="product-thumb" alt=""/>
         <button class="card-btn" onClick={() => cartIt(product)}>Add to Cart</button>
-        </div> 
+      </div>
+      <div class="product-info">
+        <h2 class="product-brand">{product.title}</h2>
+        <span class="price">${product.price}</span>
+        <span class="actual-price">${product.actual}</span>
+      </div>
     </div>
   )
 }

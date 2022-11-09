2ca3ccf2931ccd27ac885ae198263107b6d2d37a
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-09 15:40:30 +0300

    adds imports to App component

--- client/vikali/src/components/App.js
+++ client/vikali/src/components/App.js
@@ -1,10 +1,64 @@
 import logo from '../logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import Categories from "./";
+import NavBar from "./Navbar";
+import Electronics from "./Electronics";
+import Fashion from "./Fashion";
+import Groceries from "./Groceries";
+import Shoes from "./Shoes";
+import Cart from "./Cart";
 import Home from './Home'
+import './App.css';
+
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("https://drip-mall-project.herokuapp.com/products")
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function addToCart(product) {
+    if (!cart.includes(product)) {
+      setCart([...cart, product])
+      fetch("https://drip-mall-project.herokuapp.com/cart", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product)
+      });
+    }
+  }
   return (
     <div className="App">
       <Home />
+      <Announcement />
+      <NavBar searchChange={setSearch} />
+      <Categories />
+      <Switch>
+        <Route path="/" exact>
+          <Home products={products} search={search} cartIt={addToCart} />
+        </Route>
+        <Route path="/electronics">
+          <Electronics products={products} cartIt={addToCart} />
+        </Route>
+        <Route path="/fashion">
+          <Fashion products={products} cartIt={addToCart} />
+        </Route>
+        <Route path="/shoes">
+          <Shoes products={products} cartIt={addToCart} />
+        </Route>
+        <Route path="/groceries">
+          <Groceries products={products} cartIt={addToCart} />
+        </Route>
+        <Route path="/cart">
+          <Cart products={cart} />
+        </Route>
+      </Switch>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

fc5d0f3ec042463dc5694e8b5de06ce61cc54d0e
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-11 00:39:36 +0300

    adds review button to Product card

--- client/vikali/src/components/ProductCard.js
+++ client/vikali/src/components/ProductCard.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import "./Products.css"
 
-export default function ProductCard({product, cartIt}) {
+export default function ProductCard({ product, cartIt }) {
   return (
-    <div class= "product-card">
+    <div class="product-card">
       <div class="product-image">
         <span class="discount-tag">{product.discount}% off</span>
-        <img src= {product.image} class="product-thumb" alt=""/>
+        <img src={product.image} class="product-thumb" alt="" />
         <button class="card-btn" onClick={(e) => cartIt(product)}>Add to Cart</button>
       </div>
       <div class="product-info">
         <h2 class="product-brand">{product.title}</h2>
         <span class="price">${product.price}</span>
         <span class="actual-price">${product.actual_price}</span>
+        
       </div>
+      <button id='review-btn'>Add Review</button>
     </div>
   )
 }

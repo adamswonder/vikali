36cffdf42e5b776b3b129637b4a5b01e57de37c0
+ <+>
2022-11-13 18:32:44 +0300

    Edited Categories component

--- client/vikali/src/components/Categories.js
+++ client/vikali/src/components/Categories.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-
 function Categories() {
+  let activeStyle = {
+    color: "orange",
+  };
     return (
-      <nav ActiveClassName="categories">
-        <NavLink to="/app/products" ActiveClassName="current">
+      <nav className="categories">
+        <NavLink to="/app/products" style={({isActive}) => isActive ? activeStyle : undefined}>
           Home
         </NavLink>
-        <NavLink to="/app/electronics" ActiveClassName="current">
+        <NavLink to="/app/electronics" style={({isActive}) => isActive ? activeStyle : undefined}>
           Electronics
         </NavLink>
-        <NavLink to="/app/fashion" ActiveClassName="current">
+        <NavLink to="/app/fashion" style={({isActive}) => isActive ? activeStyle : undefined}>
           Fashion
         </NavLink>
-        <NavLink to="/app/shoes" ActiveClassName="current">
+        <NavLink to="/app/shoes" style={({isActive}) => isActive ? activeStyle : undefined}>
           Shoes
         </NavLink>
-        <NavLink to="/app/groceries" ActiveClassName="current">
+        <NavLink to="/app/groceries" style={({isActive}) => isActive ? activeStyle : undefined}>
           Groceries
         </NavLink>
       </nav>
